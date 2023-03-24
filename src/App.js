5ea804fbcda4a18955f20d4f6a50ea5e905fe518
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-03-23 22:15:39 -0300

    Criando o contador

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Button from "./components/Button/index";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleSubtract = this.handleSubtract.bind(this);
+    this.handlerReset = this.handlerReset.bind(this);
+  }
+
+  handleAdd() {
+    this.setState((state) => ({ counter: state.counter + 1 }));
+  }
+
+  handleSubtract() {
+    this.setState((state) => ({ counter: state.counter - 1 }));
+  }
+
+  handlerReset() {
+    this.setState({ counter: 0 });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+
+        <main>
+          <div className="display">
+            <span>{this.state.counter}</span>
+          </div>
+
+          <div className="buttons-container">
+            <Button
+              label="-1"
+              changeCounter={this.handleSubtract}
+              color="red"
+            />
+            <Button label="Zerar" changeCounter={this.handlerReset} />
+            <Button label="+1" changeCounter={this.handleAdd} color="green" />
+          </div>
+        </main>
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <span>&copy; devCounter - 2023.</span>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
